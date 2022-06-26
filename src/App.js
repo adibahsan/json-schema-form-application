127b61ba2d7d5aa532182e742386a0943a3f6191
Adib <adibahsan@iut-dhaka.edu>
2022-06-26 23:11:41 +0600

    custom styling on error

--- src/App.js
+++ src/App.js
@@ -2,32 +2,72 @@ import logo from './logo.svg';
 import './App.css';
 
 import Form from "react-jsonschema-form";
+import {type} from "@testing-library/user-event/dist/type";
 
-const schema = {
-    title: "Todo",
-    type: "object",
-    required: ["title"],
-    properties: {
-        title: {type: "string", title: "Title", default: "A new task"},
-        done: {type: "boolean", title: "Done?", default: false}
-    }
-};
+// const schema = {
+//     title: "Todo",
+//     type: "object",
+//     required: ["title"],
+//     properties: {
+//         title: {type: "string", title: "Title", default: "A new task"},
+//         done: {type: "boolean", title: "Done?", default: false}
+//     }
+// };
 
 const log = (type) => console.log.bind(console, type);
 
 
-function App() {
-  return (
-    <div className="App">
+const schema = {
+    type: "string"
+};
 
-      <div>
-        <Form schema={schema}
-              onChange={log("changed")}
-              onSubmit={log("submitted")}
-              onError={log("errors")} />
-      </div>
-    </div>
-  );
+function MyCustomWidget(props) {
+    console.log("props", props)
+    const {options, rawErrors} = props;
+    console.log("rawErrors", type(rawErrors));
+    const {color, backgroundColor, borderColor, borderWidth, width} = options;
+    return <input style={{color, backgroundColor, borderColor, borderWidth}}
+                  className={rawErrors!=null && rawErrors.length>0 ? "justError" : null}/>;
+}
+
+MyCustomWidget.defaultProps = {
+    options: {
+        color: "red",
+        backgroundColor: "pink",
+        borderColor: "purple",
+        borderWidth: 5,
+        width: 15
+    }
+};
+
+const uiSchema = {
+    "ui:widget": MyCustomWidget,
+    "ui:options": {
+        color: "black",
+        textAlign: "center",
+        backgroundColor: "yellow"
+    }
+};
+
+
+function App() {
+    return (
+        <div className="App">
+
+            <div>
+                <Form schema={schema}
+                      uiSchema={uiSchema}
+                      onChange={
+                          log("changed")
+                      }
+                      onSubmit={
+                          log("submitted")
+                      }
+                      liveValidate={true}
+                      onError={log("errors")}/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
